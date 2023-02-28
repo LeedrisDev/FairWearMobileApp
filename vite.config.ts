ba21ab6614cb7fa35669cc2bdde747cb33598a08
Felix Ngo <felix.ngo@epita.fr>
2023-02-28 23:45:41 +0100

    [PWA] Add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+// @ts-ignore
+import manifest from './public/manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+              enabled: true
+          },
+          manifest,
+      })
+  ],
 })
