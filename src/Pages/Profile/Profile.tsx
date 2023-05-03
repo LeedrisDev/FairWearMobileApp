29f8be7c2b7bb278ad8888e87cc2ab2b001ea766
Margaux Cavalié <margaux.cavalie@gmail.com>
2023-05-03 10:46:07 +0000

    Add link to profile page and reordered files

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from '../public/react.svg'
-import './App.css'
+import '../../App.css';
+import { useNavigate } from "react-router-dom";
+import profilePicture from '../../Images/profilePic.jpg';
+import beanie from '../../Images/beanie.jpeg';
+import shirt from '../../Images/shirt.jpeg';
+import jeans from '../../Images/jeans.jpeg';
+import globe from '../../Images/globe.png';
 
-function App() {
-    const [count, setCount] = useState(0)
+function Profile() {
+    let navigate = useNavigate();
 
     return (
         <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+            <button className='settings' onClick={() => { navigate('Settings') }}>Settings</button>
+            <div className='profile'>
+                <img src={profilePicture} className='profile-picture' />
+                <div className='title-one'>Margaux</div>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+            <div className='content'>
+                <div className='history'>
+                    <div className='title'>
+                        <span className='title-two'>History</span>
+                        <span className='action'>See all</span>
+                    </div>
+                    <div className='content2'>
+                        <div className='clothes'>
+                            <div className='clothe'>
+                                <img src={shirt} className='product-image' />
+                                <div className='brand-and-grade title-four'>
+                                    <span>Shirt</span>
+                                    <span className='fairscore'><img src={globe} width={15} /> 4</span>
+                                </div>
+                            </div>
+                            <div className='clothe'>
+                                <img src={beanie} className='product-image' />
+                                <div className='brand-and-grade title-four'>
+                                    <span>Beanie</span>
+                                    <span className='fairscore'><img src={globe} width={15} /> 4</span>
+                                </div>
+                            </div>
+                            <div className='clothe'>
+                                <img src={jeans} className='product-image' />
+                                <div className='brand-and-grade title-four'>
+                                    <span>Jeans</span>
+                                    <span className='fairscore'><img src={globe} width={15} /> 4</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='history'>
+                    <div className='title'>
+                        <span className='title-two'>Stats</span>
+                    </div>
+                    <div className='content2'>
+                        <div className='clothes'>
+                            Coming soon
+                        </div>
+                    </div>
+                </div>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </div>
+        </div >
     )
 }
 
-export default App
+export default Profile
