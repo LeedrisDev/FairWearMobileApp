f3fa0259f5605551e13e058d35cd927f958f43c8
Margaux Cavalié <margaux.cavalie@gmail.com>
2023-05-07 12:14:42 +0000

    Moved CSS to the right file, cleaned things

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,3 +1,4 @@
+import './Profile.css';
 import '../../App.css';
 import { useNavigate } from "react-router-dom";
 import profilePicture from '../../Images/profilePic.jpg';
@@ -11,35 +12,35 @@ function Profile() {
 
     return (
         <div className="App">
-            <button className='settings' onClick={() => { navigate('Settings') }}>Settings</button>
+            <button className='settings' onClick={() => { navigate('Settings') }}>S</button>
             <div className='profile'>
                 <img src={profilePicture} className='profile-picture' />
                 <div className='title-one'>Margaux</div>
             </div>
             <div className='content'>
-                <div className='history'>
-                    <div className='title'>
+                <div className='category'>
+                    <div className='box-header'>
                         <span className='title-two'>History</span>
                         <span className='action'>See all</span>
                     </div>
-                    <div className='content2'>
+                    <div className='box'>
                         <div className='clothes'>
-                            <div className='clothe'>
-                                <img src={shirt} className='product-image' />
+                            <div className='clothing-item'>
+                                <img src={shirt} className='item-image' />
                                 <div className='brand-and-grade title-four'>
                                     <span>Shirt</span>
                                     <span className='fairscore'><img src={globe} width={15} /> 4</span>
                                 </div>
                             </div>
-                            <div className='clothe'>
-                                <img src={beanie} className='product-image' />
+                            <div className='clothing-item'>
+                                <img src={beanie} className='item-image' />
                                 <div className='brand-and-grade title-four'>
                                     <span>Beanie</span>
                                     <span className='fairscore'><img src={globe} width={15} /> 4</span>
                                 </div>
                             </div>
-                            <div className='clothe'>
-                                <img src={jeans} className='product-image' />
+                            <div className='clothing-item'>
+                                <img src={jeans} className='item-image' />
                                 <div className='brand-and-grade title-four'>
                                     <span>Jeans</span>
                                     <span className='fairscore'><img src={globe} width={15} /> 4</span>
@@ -48,11 +49,11 @@ function Profile() {
                         </div>
                     </div>
                 </div>
-                <div className='history'>
-                    <div className='title'>
+                <div className='category'>
+                    <div className='box-header'>
                         <span className='title-two'>Stats</span>
                     </div>
-                    <div className='content2'>
+                    <div className='box'>
                         <div className='clothes'>
                             Coming soon
                         </div>
