c0e4f956068064a6cf25cd6674e1c416860b546c
mariemtrn <marie.maturana@epita.fr>
2023-05-10 16:30:03 +0200

    [Product Page] Alternatives css

--- src/Components/ProductPage/Alternatives.tsx
+++ src/Components/ProductPage/Alternatives.tsx
@@ -1,4 +1,6 @@
+import product1 from "../../assets/produit1.png";
 
+import "./Alternatives.css"
 interface IPropsAlternatives {
     alternative: string[]
 }
@@ -7,6 +9,20 @@ function Alternatives(props : IPropsAlternatives) {
     return (
         <div className="score">
             <div>Alternatives</div>
+            <div className='clothes'>
+                <div className='clothing-item'>
+                    <img src={product1} className='item-image' />
+                    <div className='brand-and-grade title-four'>
+                        <span>Shirt</span>
+                    </div>
+                </div>
+                <div className='clothing-item'>
+                    <img src={product1} className='item-image' />
+                    <div className='brand-and-grade title-four'>
+                        <span>Shirt</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
