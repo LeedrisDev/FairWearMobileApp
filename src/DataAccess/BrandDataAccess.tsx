ebff8f3b415e28f72ac2bea4e6c6d28878060a6b
Thomas Blondel <tfoenix@gmail.com>
2023-07-10 17:37:21 +0200

    [DataAccess] Added sample for request with Axios

--- src/DataAccess/BrandDataAccess.tsx
+++ src/DataAccess/BrandDataAccess.tsx
@@ -1,31 +1,43 @@
 import axios from 'axios';
 
 function postBrand(brand:string) {
-  const formdata = new FormData();
-  formdata.append('name', brand);
+  // const formdata = new FormData();
+  // formdata.append('name', brand);
+  //
+  // const config = {
+  //   method: 'post',
+  //   maxBodyLength: Infinity,
+  //   url: 'https://gateway.traefik.me/api/brand/name',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     // 'User-Agent': 'PostmanRuntime/7.32.3',
+  //     // Accept: '*/*',
+  //     // Host: 'gateway.traefik.me',
+  //     // 'Accept-Encoding': 'gzip, deflate, br',
+  //     // Connection: 'keep-alive',
+  //     // 'Access-Control-Allow-Origin': '*',
+  //   },
+  //   formdata,
+  // };
+  //
+  // axios.request(config)
+  //   .then((response) => {
+  //     console.log(JSON.stringify(response.data));
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
-  const config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://gateway.traefik.me/api/brand/name',
-    headers: {
-      'Content-Type': 'application/json',
-      'User-Agent': 'PostmanRuntime/7.32.3',
-      Accept: '*/*',
-      Host: 'gateway.traefik.me',
-      'Accept-Encoding': 'gzip, deflate, br',
-      Connection: 'keep-alive',
-        'Access-Control-Allow-Origin': '*',
-    },
-    formdata,
-  };
-
-  axios.request(config)
+  axios.post('https://gateway.traefik.me/api/brand/name', {
+    name: 'zara',
+  })
     .then((response) => {
-      console.log(JSON.stringify(response.data));
+      console.log('Response:', response.data);
+      // Handle the response data here
     })
     .catch((error) => {
-      console.log(error);
+      console.error('Error:', error);
+      // Handle errors here
     });
 }
 
