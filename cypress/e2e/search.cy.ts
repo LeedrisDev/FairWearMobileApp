3b49b56a31f8290ce5bfb7da2829851d88f46bda
Margaux Cavalié <margaux.cavalie@gmail.com>
2023-07-11 17:27:26 +0200

    [e2e] Added wait for the presentation

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,55 +1,74 @@
 describe('Tests for the search page', () => {
     it('Should check that the page is accessible', () => {
         cy.visit(Cypress.env("baseUrl") + 'Search')
+        cy.get('.passive').eq(1).click()
     })
 
     describe('Tests for the navbar', () => {
         it('Should check that the navbar is there', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.navigation-bar').should('exist')
         })
 
         it("Should check that the 'Home' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.passive').eq(0).should('exist')
             cy.get('.passive').eq(0).click()
+            cy.wait(2000)
             cy.url().should('include', '/Home')
         })
 
         it("Should check that the 'Scan' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.passive').eq(1).should('exist')
             cy.get('.passive').eq(1).click()
-            cy.url().should('include', '')
         })
 
         it("Should check that the 'Profile' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.passive').eq(2).should('exist')
             cy.get('.passive').eq(2).click()
+            cy.wait(2000)
             cy.url().should('include', '/Profile')
         })
     })
 
     it('Should check that the input area is there', () => {
         cy.visit(Cypress.env("baseUrl") + 'Search')
+        cy.get('.passive').eq(1).click()
+        cy.wait(2000)
         cy.get('.input-search').should('exist')
     })
 
     it('Should check that the item list is empty', () => {
         cy.visit(Cypress.env("baseUrl") + 'Search')
+        cy.get('.passive').eq(1).click()
+        cy.wait(2000)
         cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 0)
     })
 
     describe("Tests for the 'Search' button", () => {
         it('Should check that the button is there', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.button-search').should('exist')
         })
 
         it('Should check that the button works', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
             cy.get('.button-search').click()
+            cy.wait(2000)
             cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 5)
         })
     })
