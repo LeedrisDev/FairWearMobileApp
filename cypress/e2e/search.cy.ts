0c2b1f33df7eedff6375359aace1dd0015570b7c
felixngo <felix.ngo@epita.fr>
2023-09-23 16:56:31 +0200

    [E2E] Reduce E2E test time and save on fail

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -8,24 +8,24 @@ describe('Tests for the search page', () => {
         it('Should check that the navbar is there', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.navigation-bar').should('exist')
         })
 
         it("Should check that the 'Home' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.passive').eq(0).should('exist')
             cy.get('.passive').eq(0).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.url().should('include', '/Home')
         })
 
         it("Should check that the 'Scan' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.passive').eq(1).should('exist')
             cy.get('.passive').eq(1).click()
         })
@@ -33,10 +33,10 @@ describe('Tests for the search page', () => {
         it("Should check that the 'Profile' button works", () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.passive').eq(2).should('exist')
             cy.get('.passive').eq(2).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.url().should('include', '/Profile')
         })
     })
@@ -45,14 +45,14 @@ describe('Tests for the search page', () => {
         it('Should check that the input area is there', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.input-search').should('exist')
         })
 
         it('Should check that the item list is empty', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 0)
         })
 
@@ -82,7 +82,7 @@ describe('Tests for the search page', () => {
 
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.input-search').type('N')
             cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 1)
         })
@@ -92,7 +92,7 @@ describe('Tests for the search page', () => {
         it('Should check that the button is there', () => {
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.button-search').should('exist')
         })
 
@@ -121,14 +121,14 @@ describe('Tests for the search page', () => {
             // ).as('getSuggestions')
             cy.visit(Cypress.env("baseUrl") + 'Search')
             cy.get('.passive').eq(1).click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.input-search')
                 .type('N', {delay: 100})
                 .type('i', {delay: 100})
                 .type('k', {delay: 100})
                 .type('e', {delay: 100});
             cy.get('.button-search').click()
-            cy.wait(2000)
+            cy.wait(100)
             cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 1)
         })
     })
