01bbbc4db460444553e5be1b4c511c043ee5531a
felixngo <felix.ngo@epita.fr>
2023-09-23 16:25:40 +0200

    [E2E] Mock brand get response

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -41,19 +41,52 @@ describe('Tests for the search page', () => {
         })
     })
 
-    it('Should check that the input area is there', () => {
-        cy.visit(Cypress.env("baseUrl") + 'Search')
-        cy.get('.passive').eq(1).click()
-        cy.wait(2000)
-        cy.get('.input-search').should('exist')
-    })
+    describe("Tests for input area", () => {
+        it('Should check that the input area is there', () => {
+            cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
+            cy.get('.input-search').should('exist')
+        })
 
-    it('Should check that the item list is empty', () => {
-        cy.visit(Cypress.env("baseUrl") + 'Search')
-        cy.get('.passive').eq(1).click()
-        cy.wait(2000)
-        cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 0)
-    })
+        it('Should check that the item list is empty', () => {
+            cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
+            cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 0)
+        })
+
+        it('Should check that the search suggestion works', () => {
+            cy.intercept(
+                {
+                    method: 'GET',
+                    url: '**/brand*',
+                },
+                {
+                    statusCode: 200,
+                    body:
+                        [{
+                            'id': 1,
+                            'name': "Nike",
+                            'country': "USA",
+                            'environmentRating': 4,
+                            'peopleRating': 5,
+                            'animalRating': 5,
+                            'ratingDescription': "It's a good brand",
+                            'categories': [],
+                            'ranges': []
+                        }]
+                    ,
+                }
+            ).as('getSuggestions') // and assign an alias
+
+            cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
+            cy.get('.input-search').type('N')
+            cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 1)
+        })
+    });
 
     describe("Tests for the 'Search' button", () => {
         it('Should check that the button is there', () => {
@@ -63,13 +96,40 @@ describe('Tests for the search page', () => {
             cy.get('.button-search').should('exist')
         })
 
-        // it('Should check that the button works', () => {
-        //     cy.visit(Cypress.env("baseUrl") + 'Search')
-        //     cy.get('.passive').eq(1).click()
-        //     cy.wait(2000)
-        //     cy.get('.button-search').click()
-        //     cy.wait(2000)
-        //     cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 5)
-        // })
+        it('Should check that the button works', () => {
+            cy.intercept(
+                {
+                    method: 'GET',
+                    url: '**/brand*',
+                },
+                {
+                    statusCode: 200,
+                    body:
+                        [{
+                            'id': 1,
+                            'name': "Nike",
+                            'country': "USA",
+                            'environmentRating': 4,
+                            'peopleRating': 5,
+                            'animalRating': 5,
+                            'ratingDescription': "It's a good brand",
+                            'categories': [],
+                            'ranges': []
+                        }]
+                    ,
+                }
+            ).as('getSuggestions')
+            cy.visit(Cypress.env("baseUrl") + 'Search')
+            cy.get('.passive').eq(1).click()
+            cy.wait(2000)
+            cy.get('.input-search')
+                .type('N', {delay: 100})
+                .type('i', {delay: 100})
+                .type('k', {delay: 100})
+                .type('e', {delay: 100});
+            cy.get('.button-search').click()
+            cy.wait(2000)
+            cy.get('.clothing-item-alternatives').should('not.be.empty').and('have.length', 1)
+        })
     })
 })
