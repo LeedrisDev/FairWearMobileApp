073a1a2489aec9d1f731d7c7b46f54264f648bc0
felix <felix7051@gmail.com>
2023-10-12 23:50:23 +0200

    [CONFIG] Add env vars for firebase

--- src/Utils/AppConstants.ts
+++ src/Utils/AppConstants.ts
@@ -1,5 +1,14 @@
 const AppConstants = {
   API_HOST: import.meta.env.VITE_API_HOST || 'https://gateway.traefik.me',
+  FIREBASE_CONFIG: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  },
 };
 
 export default AppConstants;
