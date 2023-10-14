2225d918640592bf78f269cb2f87d6794a7e1292
felix <felix7051@gmail.com>
2023-10-14 19:36:59 +0200

    [SIGNUP] refacto signup page to multi form page

--- src/Pages/Authentication/SignUp/SignupPage.tsx
+++ src/Pages/Authentication/SignUp/SignupPage.tsx
@@ -3,8 +3,12 @@ import 'firebaseui/dist/firebaseui.css';
 import auth from '../../../Utils/Auth';
 import { Button, Form } from 'react-bootstrap';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-// import "../../../index.scss"
+import {Link, useNavigate} from 'react-router-dom';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import bigLogo from '../../../assets/FairWearBig.png';
+import './SignupPage.css';
+
 
 interface SignUpFormStateProps {
   username: string;
@@ -24,6 +28,7 @@ export default function SignupPage() {
     password: '',
     confirmPassword: '',
   });
+  const [nextPage, setNextPage] = useState(false);
 
   const [validated, setValidated] = useState(false);
 
@@ -51,77 +56,117 @@ export default function SignupPage() {
     setValidated(true);
   };
 
+  const handleNextPage = () => {
+    if (signUpFormState.email !== "" && signUpFormState.username !== ""
+        && signUpFormState.phone !== "")
+    {
+      setNextPage(true);
+    }
+  }
+
+  const handleBack = () => {
+    if (nextPage) {
+      setNextPage(false);
+    }
+    else {
+      navigate('/Auth');
+    }
+  }
+
   return (
-    <div>
-      <h1 className="text-center my-3 title">Signup</h1>
-      <Form noValidate validated={validated} onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="usernameField">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Enter username"
-            value={signUpFormState.username}
-            onChange={(event) => setSignUpState(
-              { ...signUpFormState, username: event.target.value },
-            )}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="emailField">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            required
-            type="email"
-            placeholder="Enter email"
-            value={signUpFormState.email}
-            onChange={(event) => setSignUpState(
-              { ...signUpFormState, email: event.target.value },
-            )}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="phoneField">
-          <Form.Label>Phone number</Form.Label>
-          <Form.Control
-            required
-            type="tel"
-            placeholder="Enter phone"
-            value={signUpFormState.phone}
-            onChange={(event) => setSignUpState(
-              { ...signUpFormState, phone: event.target.value },
-            )}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="passwordField">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            required
-            type="password"
-            placeholder="Enter password"
-            value={signUpFormState.password}
-            onChange={(event) => setSignUpState(
-              { ...signUpFormState, password: event.target.value },
-            )}
-          />
-          <Form.Label>Confirm password</Form.Label>
-          <Form.Control
-            required
-            type="password"
-            placeholder="Confirm password"
-            value={signUpFormState.confirmPassword}
-            disabled={signUpFormState.password === ''}
-            onChange={(event) => setSignUpState(
-              { ...signUpFormState, confirmPassword: event.target.value },
-            )}
-            isValid={signUpFormState.password === signUpFormState.confirmPassword}
-          />
-          <Form.Control.Feedback type="invalid">
-            Passwords don&apos;t match
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          SignUp
-        </Button>
-      </Form>
+    <div className="signup-page" style={{ width: window.innerWidth, height: window.innerHeight }}>
+    <div className="nav-link back" onClick={handleBack}>
+      <FontAwesomeIcon icon={faArrowLeft} className="icon-navigation" />
+    </div>
+
+      <div className="main-container">
+        <div className="logo-container">
+          <img className="logo" src={bigLogo} alt={bigLogo} />
+        </div>
+
+        <Form noValidate validated={validated} onSubmit={onSubmit}>
+
+          <div className={nextPage ? "" : "signup-form"} hidden={nextPage}>
+            <Form.Group className="form-group" controlId="usernameField">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                className="form-control "
+                required
+                type="text"
+                placeholder="Enter username"
+                value={signUpFormState.username}
+                onChange={(event) => setSignUpState(
+                  { ...signUpFormState, username: event.target.value },
+                )}
+              />
+            </Form.Group>
+            <Form.Group className="form-group" controlId="emailField">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                className="form-control"
+                required
+                type="email"
+                placeholder="Enter email"
+                value={signUpFormState.email}
+                onChange={(event) => setSignUpState(
+                  { ...signUpFormState, email: event.target.value },
+                )}
+              />
+            </Form.Group>
+            <Form.Group className="form-group" controlId="phoneField">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control
+                className="form-control"
+                required
+                type="tel"
+                placeholder="Enter phone"
+                value={signUpFormState.phone}
+                onChange={(event) => setSignUpState(
+                  { ...signUpFormState, phone: event.target.value },
+                )}
+              />
+            </Form.Group>
+            <Button className="signup-button" onClick={handleNextPage}>
+              <span>NEXT</span>
+            </Button>
+          </div>
+
+          <div className={nextPage ? "signup-form" : ""} hidden={!nextPage}>
+            <Form.Group className="form-group" controlId="passwordField">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                className="form-control"
+                required
+                type="password"
+                placeholder="Enter password"
+                value={signUpFormState.password}
+                onChange={(event) => setSignUpState(
+                  { ...signUpFormState, password: event.target.value },
+                )}
+              />
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                className="form-control"
+                required
+                type="password"
+                placeholder="Confirm password"
+                value={signUpFormState.confirmPassword}
+                disabled={signUpFormState.password === ''}
+                onChange={(event) => setSignUpState(
+                  { ...signUpFormState, confirmPassword: event.target.value },
+                )}
+                isValid={signUpFormState.password === signUpFormState.confirmPassword}
+              />
+              {/* <Form.Control.Feedback type="invalid"> */}
+              {/*   <span>Passwords don&apos;t match</span> */}
+              {/* </Form.Control.Feedback> */}
+            </Form.Group>
+            <Button className="button-container signup-button" type="submit">
+              SIGN UP
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
